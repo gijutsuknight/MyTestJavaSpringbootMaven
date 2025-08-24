7e63ec38d37a46f7f6d79902edf439161e2dbeb3
Yeat Nai Cheng <yeatnaicheng1@gmail.com>
2025-08-24 11:02:29 +0800

    Add context path, updated testcontroller

--- src/main/java/my/test/java/controller/TestController.java
+++ src/main/java/my/test/java/controller/TestController.java
@@ -1,14 +1,47 @@
 package my.test.java.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/test")
 public class TestController {
-    @GetMapping("/getTest")
-    public String getTest(){
-        return "Test Result";
+    @GetMapping("/string")
+    public String getString(){
+        return "OK";
     }
+
+    @GetMapping("/client-error/400")
+    public ResponseEntity<Void> getError400(){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @GetMapping("/client-error/404")
+    public ResponseEntity<Void> getError404(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/server-error/500")
+    public ResponseEntity<Void> getError500(){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+
+    @GetMapping("/delay-5-seconds")
+    public String getStringAfter5Seconds() throws InterruptedException {
+        Thread.sleep(5000);
+        return "OK";
+    }
+
+    @GetMapping("/delay-10-seconds")
+    public String getStringAfter10Seconds() throws InterruptedException {
+        Thread.sleep(5000);
+        return "OK";
+    }
+
+
 }
